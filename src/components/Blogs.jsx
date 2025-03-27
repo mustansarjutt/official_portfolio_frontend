31a9048d2cdd448e5bd5b3b463aa032e15f80609
Mustansar Jutt <mhgjutt@gmail.com>
2025-03-27 14:35:17 +0500

    Update home page

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,7 +1,7 @@
 export default function Blogs() {
     return (
-        <div>
-            Blogs
+        <div className="min-h-4/5 h-auto flex justify-center items-center">
+            <h1 className="text-3xl text-gray-700">Comming soon...</h1>
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { FaNodeJs, FaHtml5, FaCss3, FaGithub } from "react-icons/fa"
 import { SiJavascript, SiTailwindcss, SiMongodb, SiPostman, SiFirebase, SiGit } from "react-icons/si"
 import { motion } from "framer-motion"
 import { useNavigate } from "react-router-dom"
+import pic from "../assets/me1.png"
 
 export default function Home() {
     const boxVariants1 = {
@@ -29,93 +30,108 @@ export default function Home() {
     const navigate = useNavigate()
 
     return (
-        <div className="flex justify-around p-6 md:flex-row flex-col items-center h-4/5 bg-[url('./assets/bg5.jpg')] bg-cover">
-            <div className="flex flex-col gap-y-4">
-                <motion.div
-                    variants={textVariants}
-                    initial="hidden"
-                    whileInView="visible"
-                    whileHover="hover"
-                    viewport={{ once: true }}
-                    className="text-3xl"
-                >
-                    Hii! I am{" "}
-                    <motion.span
-                        className="font-bold text-orange-500"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
+        <div className="flex flex-col gap-y-6 p-6 min-h-4/5 h-auto bg-[url('./assets/bg5.jpg')] bg-cover">
+            <div className="flex justify-around md:flex-row flex-col items-center gap-5">
+                <div className="flex flex-col gap-y-4">
+                    <motion.div
+                        variants={textVariants}
+                        initial="hidden"
+                        whileInView="visible"
+                        whileHover="hover"
+                        viewport={{ once: true }}
+                        className="text-3xl text-center"
                     >
-                        Mustansar Gill
-                    </motion.span>
-                </motion.div>
-                <motion.p
-                    variants={textVariants}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true }}
-                    className="text-3xl text-gray-700"
-                >
-                    MERN Stack Developer | Freelancer
-                </motion.p>
-                <div className="flex flex-row justify-center gap-x-3">
-                    <motion.button
-                        whileHover={{ scale: 1.1, backgroundColor: "#00897B" }} // Lighter Teal on Hover
-                        whileTap={{ scale: 0.9 }}
-                        className="bg-teal-700 px-3 py-1.5 text-white font-semibold rounded-2xl cursor-pointer transition-all"
+                        Hii! I am{" "}
+                        <motion.span
+                            className="font-bold text-orange-500"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
+                        >
+                            Mustansar Gill
+                        </motion.span>
+                    </motion.div>
+                    <motion.p
+                        variants={textVariants}
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true }}
+                        className="text-3xl text-gray-700 text-center"
                     >
-                        Download CV
-                    </motion.button>
-                    <motion.button
-                        whileHover={{ scale: 1.1, backgroundColor: "#F57C00" }} // Darker Orange on Hover
-                        whileTap={{ scale: 0.9 }}
-                        className="bg-gradient-to-r from-orange-400 to-orange-600 px-3 py-1.5 text-white font-semibold rounded-2xl cursor-pointer transition-all"
-                        onClick={() => navigate("/contact-us")}
-                    >
-                        Contact Me
-                    </motion.button>
+                        MERN Stack Developer | Freelancer
+                    </motion.p>
+                    <div className="flex flex-row justify-center gap-x-3">
+                        <motion.button
+                            whileHover={{ scale: 1.1, backgroundColor: "#00897B" }} // Lighter Teal on Hover
+                            whileTap={{ scale: 0.9 }}
+                            className="bg-teal-700 px-3 py-1.5 text-white font-semibold rounded-2xl cursor-pointer transition-all"
+                        >
+                            Download CV
+                        </motion.button>
+                        <motion.button
+                            whileHover={{ scale: 1.1, backgroundColor: "#F57C00" }} // Darker Orange on Hover
+                            whileTap={{ scale: 0.9 }}
+                            className="bg-gradient-to-r from-orange-400 to-orange-600 px-3 py-1.5 text-white font-semibold rounded-2xl cursor-pointer transition-all"
+                            onClick={() => navigate("/contact-us")}
+                        >
+                            Contact Me
+                        </motion.button>
+                    </div>
+                </div>
+                <div className="flex justify-center items-center">
+                    <motion.img
+                        src={pic}
+                        alt="me"
+                        className="md:w-72 w-56 rounded-4xl border-[4px] border-[#574964] shadow-2xl cursor-pointer"
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ type: "spring", stiffness: 100, delay: 0.5 }}
+                    />
                 </div>
             </div>
-            <div className="text-3xl font-medium flex flex-col justify-around gap-y-5">
-                <h1 className="text-slate-700 font-mono font-semibold shadow-2xl">Skills & Tools</h1>
-
-                <motion.div
-                    variants={boxVariants1}
-                    initial="hidden"
-                    whileInView="visible"
-                    whileHover="hover"
-                    viewport={{ once: true }}
-                    className="flex justify-around gap-3 border-[2px] rounded-xl p-3 bg-gray-200 border-gray-500 cursor-pointer h-20 items-center"
-                >
-                    <div className="text-[#F7DF1E]"><SiJavascript /></div>
-                    <div className="text-[#1572B6]"><FaCss3 /></div>
-                    <div className="text-[#E34F26]"><FaHtml5 /></div>
-                </motion.div>
-                <motion.div
-                    variants={boxVariants2}
-                    initial="hidden"
-                    whileInView="visible"
-                    whileHover="hover"
-                    viewport={{ once: true }}
-                    className="flex justify-around gap-3 border-[2px] rounded-xl p-3 bg-gray-200 border-gray-500 cursor-pointer h-20 items-center"
-                >
-                    <div className="text-[#8CC84B]"><FaNodeJs /></div>
-                    <div className="text-[#38B2AC]"><SiTailwindcss /></div>
-                    <div className="text-[#47A248]"><SiMongodb /></div>
-                </motion.div>
-                <motion.div
-                    variants={boxVariants3}
-                    initial="hidden"
-                    whileInView="visible"
-                    whileHover="hover"
-                    viewport={{ once: true }}
-                    className="flex justify-around gap-3 border-[2px] rounded-xl p-3 bg-gray-200 border-gray-500 cursor-pointer h-20 items-center"
-                >
-                    <div className="text-[#181717]"><FaGithub /></div>
-                    <div className="text-[#F05032]"><SiGit /></div>
-                    <div className="text-[#FFCA28]"><SiFirebase /></div>
-                    <div className="text-[#FF6C37]"><SiPostman /></div>
-                </motion.div>
+            <hr className="border-2 border-slate-900 w-full" />
+            <div className="flex justify-center items-center flex-col gap-y-4">
+                <h1 className="text-3xl font-sans font-bold text-green-800">Skills & Tools</h1>
+                <div className="md:w-[60%] sm:w-[70%] w-[90%] flex justify-around items-center flex-wrap flex-row border-2 border-slate-300 rounded-3xl text-3xl py-5 bg-slate-300/35 gap-3.5">
+                    <motion.div
+                        variants={boxVariants1}
+                        initial="hidden"
+                        whileInView="visible"
+                        whileHover="hover"
+                        viewport={{ once: true }}
+                        className="w-[180px] flex justify-around gap-3 border-[2px] rounded-xl p-3 bg-gray-200 border-gray-500 cursor-pointer h-20 items-center"
+                    >
+                        <div className="text-[#F7DF1E]"><SiJavascript /></div>
+                        <div className="text-[#1572B6]"><FaCss3 /></div>
+                        <div className="text-[#E34F26]"><FaHtml5 /></div>
+                    </motion.div>
+                    <motion.div
+                        variants={boxVariants2}
+                        initial="hidden"
+                        whileInView="visible"
+                        whileHover="hover"
+                        viewport={{ once: true }}
+                        className="w-[180px] flex justify-around gap-3 border-[2px] rounded-xl p-3 bg-gray-200 border-gray-500 cursor-pointer h-20 items-center"
+                    >
+                        <div className="text-[#8CC84B]"><FaNodeJs /></div>
+                        <div className="text-[#38B2AC]"><SiTailwindcss /></div>
+                        <div className="text-[#47A248]"><SiMongodb /></div>
+                    </motion.div>
+                    <motion.div
+                        variants={boxVariants3}
+                        initial="hidden"
+                        whileInView="visible"
+                        whileHover="hover"
+                        viewport={{ once: true }}
+                        className="w-[180px] flex justify-around gap-3 border-[2px] rounded-xl p-3 bg-gray-200 border-gray-500 cursor-pointer h-20 items-center"
+                    >
+                        <div className="text-[#181717]"><FaGithub /></div>
+                        <div className="text-[#F05032]"><SiGit /></div>
+                        <div className="text-[#FFCA28]"><SiFirebase /></div>
+                        <div className="text-[#FF6C37]"><SiPostman /></div>
+                    </motion.div>
+                </div>
             </div>
         </div>
     )
